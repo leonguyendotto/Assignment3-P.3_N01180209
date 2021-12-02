f9e9c34a19bbea5b972334c293ca87a5fab35b10
leonguyendotto <leonguyen.to@MSI>
2021-12-02 15:33:26 -0500

    Adding View/Delete

--- Assignment3-P.2_N01180209/Controllers/TeacherController.cs
+++ Assignment3-P.2_N01180209/Controllers/TeacherController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Assignment3_P._2_N01180209.Models;
 
 namespace Assignment3_P._2_N01180209.Controllers
 {
@@ -12,6 +13,34 @@ namespace Assignment3_P._2_N01180209.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        //GET: /Teacher/List
+        public ActionResult List(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers = controller.TeacherList(SearchKey);
+            return View(Teachers);
+        }
+
+
+
+        //GET: /Teacher/Show/{id}
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+            return View(NewTeacher);
+        }
+
+
+        //GET: /Teacher/DeleteConfirm/{id}
+        public ActionResult DeleteConfirm(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+
+
+            return View(NewTeacher);
         }
     }
 }
